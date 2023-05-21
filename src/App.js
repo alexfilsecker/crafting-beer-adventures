d92590bc23358373d3cd1404ca35b3d7ad6cba75
Alex <alexfilsecker@gmail.com>
2023-05-21 02:48:03 -0400

    aaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./styles/App.css";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
-  return <div className="App">hola</div>;
+  return (
+    <div className="bg-fondo1">
+      <NavBar />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
